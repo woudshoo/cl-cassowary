c737c3a30fe88cb18731c661b9abd0cd71ea836b
Willem Rein Oudshoorn <woudshoo+github@xs4all.nl>
2012-02-25 15:58:43 +0100

    Updated package.lisp with exports and changed title in README.org so it does not disappear

--- package.lisp
+++ package.lisp
@@ -1,5 +1,21 @@
 ;;;; package.lisp
 
 (defpackage #:cassowary
-  (:use #:cl))
+  (:use #:cl)
+  (:export #:init
+	   #:shutdown
+	   #:create-simplex-solver
+	   #:variable-new
+	   #:variable-lookup
+	   #:variable-value
+	   #:solver-solve
+	   #:solver-resolve
+	   #:constraint-parse
+	   #:variable-nil-p
+	   #:constraint-satisfied-p
+	   #:constraint-remove
+	   #:solver
+	   #:solver-add-strong-stay
+	   #:solver-set-edited-value
+	   #:solver-set-changed-callback))
 
